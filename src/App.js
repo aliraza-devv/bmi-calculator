60a9f2735319b0915f3b6f02c27e6d83d189be32
Ali Raza <aliraza.dev.web@gmail.com>
2022-11-02 21:30:11 +0500

    UI design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      BMI Calculator
+ 
+ return(
+  <div className="app">
+    <div className='container'>
+      <h2 className='center'>BMI Calculator</h2>
+      <form>
+ 
+        <div>
+          <label>Weight (lbs)</label>
+          <input />
+        </div>
+ 
+        <div>
+          <label>Height (in)</label>
+          <input />
+        </div>
+ 
+        <div>
+          <button className='btn' type='submit'>Submit</button>
+          <button className='btn btn-outline' type='submit'>Reload</button>
+        </div>
+      </form>
+ 
+      <div className='center'>
+        <h3>Your BMI is: </h3>
+        <p>Message</p>
+      </div>
     </div>
-  );
+  </div>
+ )
 }
 
 export default App;
